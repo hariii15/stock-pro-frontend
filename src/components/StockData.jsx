91cb14c3c25b48503f32c223bd826d00206a07c3
Your GitHub Username <your.github@email.com>
2025-01-31 22:33:12 +0530

    stock service error fixed

--- src/components/StockData.jsx
+++ src/components/StockData.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
-import StockService from '../services/StockService.js';
+import StockService from '../services/stockService.js';
 import api from '../api/axios';
 import '../styles/stock-data.css';
 import { Line } from 'react-chartjs-2';
